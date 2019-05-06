acf536f479d7dd7fdb2e7afcb393807e109d03be
blizzardsolution <48500079+blizzardsolution@users.noreply.github.com>
2019-05-06 15:35:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/sms', (req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
-});
+})
 
 app.get('/sms', (req, res) => {
     res.set('Content-Type', 'text/html')
@@ -21,7 +21,7 @@ app.get('/sms', (req, res) => {
         <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
         <h2>Go to <a href="/about">/about</a></h2>
     `)
-});
+})
 
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
