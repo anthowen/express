1a44ee715543b435b9e5c45b90679b7e7488a12a
Sergey <sergey67blizzard@yahoo.com>
2019-05-06 15:34:10 +1000

    added * route

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.get('/sms', (req, res) => {
     `)
 });
 
-//app.get('*', (req, res) => {
-//    res.set('Content-Type', 'text/html')
-//    res.status(200).send(`
-//        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-//        <h2>Go to <a href="/about">/about</a></h2>
-//    `)
-//})
+app.get('*', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.status(200).send(`
+        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+        <h2>Go to <a href="/about">/about</a></h2>
+    `)
+})
 
 module.exports = app
