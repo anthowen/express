a1aa49de4f3dc5ef0c26c1b882fb6a68ceccbccb
Sergey <sergey67blizzard@yahoo.com>
2019-05-06 15:22:57 +1000

    Twilio added

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const express = require('express')
+const http = require('http');
+const express = require('express');
 const helmet = require('helmet')
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express()
-
+const app = express();
 // add some security-related headers to the response
 app.use(helmet())
 
-app.get('*', (req, res) => {
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+app.get('/sms', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
         <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
         <h2>Go to <a href="/about">/about</a></h2>
     `)
-})
+});
+
+//app.get('*', (req, res) => {
+//    res.set('Content-Type', 'text/html')
+//    res.status(200).send(`
+//        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+//        <h2>Go to <a href="/about">/about</a></h2>
+//    `)
+//})
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
 
 module.exports = app
